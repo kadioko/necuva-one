977903d0775e61fa6524fcbc93b2bd28d82c9db7
kadioko <87696613+kadioko@users.noreply.github.com>
2026-08-07 00:01:26 +0300

    chore(deps): refresh supported dependencies

--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -10,27 +10,15 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Laptop, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 
 const ThemeSwitcher = () => {
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
-
-  // useEffect only runs on the client, so now we can safely show the UI
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
-
   const ICON_SIZE = 16;
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size={"sm"}>
+        <Button aria-label="Change theme" size="sm" variant="ghost">
           {theme === "light" ? (
             <Sun
               key="light"
@@ -54,7 +42,7 @@ const ThemeSwitcher = () => {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-content" align="start">
         <DropdownMenuRadioGroup
-          value={theme}
+          value={theme ?? "system"}
           onValueChange={(e) => setTheme(e)}
         >
           <DropdownMenuRadioItem className="flex gap-2" value="light">
